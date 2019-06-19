8cb73ad96be3b5f0f064274e0efda50627ad9462
Konrad <k.sadel@getprintbox.com>
2019-06-19 09:27:42 +0200

    migration

--- src/Migrations/Version20190615115111.php
+++ src/Migrations/Version20190615115111.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
-use App\Entity\DocumentType;
-use App\Entity\PaymentMethod;
 use App\Entity\Tax;
 use App\Entity\Util;
+use App\Entity\DocumentType;
+use App\Entity\PaymentMethod;
+use Doctrine\ORM\EntityManager;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use Doctrine\ORM\EntityManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -25,59 +25,54 @@ final class Version20190615115111 extends AbstractMigration implements Container
 
     public function up(Schema $schema) : void
     {
-        $documentType = new DocumentType();
-        $documentType->setName('invoice');
-        $this->getEntityManager()->persist($documentType);
+        $documentTypes = ['invoice'];
+        $this->addItems(DocumentType::class, $documentTypes);
 
         $paymentMethods = ['przelew', 'gotówka'];
-        foreach ($paymentMethods as $paymentMethodName) {
-            $paymentMethod = new PaymentMethod();
-            $paymentMethod->setName($paymentMethodName);
-
-            $this->getEntityManager()->persist($paymentMethod);
-        }
+        $this->addItems(PaymentMethod::class, $paymentMethods);
 
         $taxes = [23,8,5,0];
-        foreach ($taxes as $tax) {
-            $taxObj = new Tax();
-            $taxObj->setName($tax . '%');
-            $taxObj->setValue($tax);
-
-            $this->getEntityManager()->persist($taxObj);
-        }
+        $this->addItems(Tax::class, $taxes);
 
         $utils = ['usł.', 'szt.', 'godz.', 'dni'];
-        foreach ($utils as $util) {
-            $utilObj = new Util();
-            $utilObj->setName($util);
-
-            $this->getEntityManager()->persist($utilObj);
-        }
-
-        $this->getEntityManager()->flush();
+        $this->addItems(Util::class, $utils);
     }
 
     public function down(Schema $schema) : void
     {
-        $documentType = $this->getEntityManager()
-            ->getRepository(DocumentType::class)
-            ->findOneBy(['name' => 'invoice']);
-
-        $this->getEntityManager()->remove($documentType);
-
-        $paymentMethods = $this->getEntityManager()
-            ->getRepository(PaymentMethod::class)
-            ->findAll();
-
-        foreach ($paymentMethods as $paymentMethod) {
-            $this->getEntityManager()->remove($paymentMethod);
-        }
-
-        $this->getEntityManager()->flush();
+        $this->deleteItems(Tax::class);
+        $this->deleteItems(Util::class);
+        $this->deleteItems(DocumentType::class);
+        $this->deleteItems(PaymentMethod::class);
     }
 
     private function getEntityManager(): EntityManager
     {
         return $this->entityManager;
+    }
+
+    private function addItems(string $entityName, array $items): void
+    {
+        foreach ($items as $item) {
+            $object = new $entityName();
+            $object->setName($item);
+
+            $this->getEntityManager()->persist($object);
+        }
+
+        $this->getEntityManager()->flush();
+    }
+
+    private function deleteItems(string $entityName): void
+    {
+        $items = $this->getEntityManager()
+            ->getRepository($entityName)
+            ->findAll();
+
+        foreach ($items as $item) {
+            $this->getEntityManager()->remove($item);
+        }
+
+        $this->getEntityManager()->flush();
     }
 }
