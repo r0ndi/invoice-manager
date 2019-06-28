f4b2ded0333726c1f41f8f1cac6103721386c801
Konrad <k.sadel@getprintbox.com>
2019-06-28 14:19:32 +0200

    document manage

--- src/Repository/DocumentRepository.php
+++ src/Repository/DocumentRepository.php
@@ -93,6 +93,70 @@ class DocumentRepository extends ServiceEntityRepository
         return $document;
     }
 
+    public function editFromForm(FormInterface $form, Document $document, User $user): ?Document
+    {
+        $buyer = $this->getEntityManager()->find(Contractor::class, $form->get('buyer')->getData());
+        $seller = $this->getEntityManager()->find(Contractor::class, $form->get('seller')->getData());
+        $documentType = $this->getEntityManager()->find(DocumentType::class, $form->get('documentType')->getData());
+        $paymentMethod = $this->getEntityManager()->find(PaymentMethod::class, $form->get('paymentMethod')->getData());
+        $positionUtil = $this->getEntityManager()->find(Util::class, $form->get('positionUtil')->getData());
+        $positionTax = $this->getEntityManager()->find(Tax::class, $form->get('positionTax')->getData());
+
+        if (!($buyer instanceof Contractor)) {
+            return null;
+        }
+
+        if (!($seller instanceof Contractor)) {
+            return null;
+        }
+
+        if (!($documentType instanceof DocumentType)) {
+            return null;
+        }
+
+        if (!($paymentMethod instanceof PaymentMethod)) {
+            return null;
+        }
+
+        if (!($positionUtil instanceof Util)) {
+            return null;
+        }
+
+        if (!($positionTax instanceof Tax)) {
+            return null;
+        }
+
+        $document->setUser($user);
+        $document->setStatus(true);
+        $document->setBuyer($buyer);
+        $document->setSeller($seller);
+        $document->setDateAdded(new DateTime());
+        $document->setDocumentType($documentType);
+        $document->setPaymentMethod($paymentMethod);
+        $document->setTitle($form->get('title')->getData());
+        $document->setBankNo($form->get('bankNo')->getData());
+        $document->setPaid((bool)$form->get('paid')->getData());
+        $document->setPlaceIssue($form->get('placeIssue')->getData());
+        $document->setDateSell(new DateTime($form->get('dateSell')->getData()));
+        $document->setDateIssue(new DateTime($form->get('dateIssue')->getData()));
+        $document->setPaymentDateLimit(new DateTime($form->get('paymentDateLimit')->getData()));
+
+        $this->getEntityManager()->persist($document);
+
+        $documentPosition = $document->getPositions()->first();
+        $documentPosition->setDocument($document);
+        $documentPosition->setUtil($positionUtil);
+        $documentPosition->setTax($positionTax);
+        $documentPosition->setName($form->get('positionName')->getData());
+        $documentPosition->setQuantity($form->get('positionQuantity')->getData());
+        $documentPosition->setPrice($form->get('positionNetPrice')->getData());
+
+        $this->getEntityManager()->persist($documentPosition);
+        $this->getEntityManager()->flush();
+
+        return $document;
+    }
+
     public function changeStatus(Document $document): Document
     {
         $document->setStatus(!$document->getStatus());
